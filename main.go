9fb637df1cc35f24114b35d0ea333aa1d199dcca
Zero <bro@coolness.com>
2025-06-23 07:59:19 +0500

    Built the server and added health check

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/gitgud5/shiny-octo-train/internal/app"
 )
 
@@ -13,4 +17,22 @@ func main() {
 
 	app.Logger.Println("We are running our appliction")
 
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	http.HandleFunc("/health", HealthCheck)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal("Something happen while running the server")
+	}
+
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Status is available")
 }
